18d3fc2da8e6f8c289f2f8736aed5dd966529686
ayrc1920-wq <ayrc19.20@gmail.com>
2026-09-09 00:37:47 +0000

    Solucion taller 1

---
+++ taller1.py
@@ -0,0 +1,12 @@
+#Taller:
+# 1 Preg
+# RPT: A₂,₃ = 0. Como el índice inicia en 0, corresponde a la tercera fila y cuarta columna
+#  de la matriz. El valor 0 representa un píxel negro en la representación visuaL
+#
+# 2 Preg
+# RPT:Un tensor RGB de 1080 × 1920 × 3 contiene 1080 × 1920 píxeles y 3 valores por cada píxel: rojo, verde y azul. Como cada valor ocupa 1 byte,
+# se almacenan 1080 × 1920 × 3 = 6.220.800 bytes, aproximadamente 6,22 MB sin compresión. 
+
+import numpy as np
+# matriz original 3x3 muy oscura
+
